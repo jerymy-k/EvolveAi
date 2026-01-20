f2510c2f427b10f37f4d8989b6051509e7a6e241
Omar EL FILALI <omarelfiie2007@gmail.com>
2026-01-20 12:22:50 +0100

    I added the 'survey_respsonses' and 'user_skills' table and removed the 'user_data' table

--- database/database.sql
+++ database/database.sql
@@ -6,11 +6,6 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT NOW()
 );
 
-CREATE TABLE user_data (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id,
-    objective,
-);
 
 CREATE TABLE survey_responses (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -22,7 +17,6 @@ CREATE TABLE survey_responses (
     work_schedule VARCHAR(50) NOT NULL,
     ai_confidence VARCHAR(50) NOT NULL,
     daily_time_investment VARCHAR(20) NOT NULL,
-
     -- Phase 2 (post-signup, optional)
     financial_feeling VARCHAR(50),
     work_issues TEXT[],              -- multi-select
@@ -34,6 +28,37 @@ CREATE TABLE survey_responses (
     CONSTRAINT fk_survey_user
         FOREIGN KEY (user_id)
         REFERENCES user(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE user_skills (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id INT NOT NULL,
+    skill_name VARCHAR(50),
+    mastery SMALLINT NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+    
+    CONSTRAINT fk_skills_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE opportunities (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(150) NOT NULL,
+    description TEXT,
+    required_skill VARCHAR(100),
+    money_gain NUMERIC(12,2),
+    link VARCHAR(255),
+    status ENUM('not_started', 'in_progress', 'completed') NOT NULL DEFAULT 'not_started',
+    created_at TIMESTAMP DEFAULT NOW(),
+
+    CONSTRAINT fk_opportunity_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(id)
         ON DELETE CASCADE
 );
 
