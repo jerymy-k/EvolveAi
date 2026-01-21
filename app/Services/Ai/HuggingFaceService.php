09c5e59569b62d83fb90a04cf81e875fb9184ec5
bahaztariq <bahaztariq@gmail.com>
2026-01-21 09:04:34 +0100

    fix some issues

--- app/Services/Ai/HuggingFaceService.php
+++ app/Services/Ai/HuggingFaceService.php
@@ -82,7 +82,7 @@ header('Content-Type: application/json');
 $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, true);
 
-$message = 'Hello';
+$message = $input['text'] ?? '';
 $history = $input['history'] ?? [];
 
 if ($message) {
